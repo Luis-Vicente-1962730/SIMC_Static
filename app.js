493fd6db65714b500acc4c41123f2b2b0837a793
Luis-Vicente-1962730 <vicente.gomezmln@uanl.edu.mx>
2026-07-29 10:17:45 -0600

    Actualizacion de la interfaz y marcas

--- app.js
+++ app.js
@@ -6,3 +6,26 @@ document.addEventListener("DOMContentLoaded", () => {
     navMenu.classList.toggle("show");
   });
 });
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+const messages = [
+  "Distribuidor autorizado de marcas líderes.",
+  "19 años impulsando la automatización industrial",
+  "Cobertura y envíos a todo México"
+];
+
+let current = 0;
+
+const message = document.getElementById("message-slider");
+
+setInterval(()=>{
+  message.style.opacity = 0;
+  setTimeout(()=>{
+    current = (current+1) % messages.length;
+    message.textContent = messages[current];
+    message.style.opacity = 1;
+  },400);
+},3500);
+
+});
